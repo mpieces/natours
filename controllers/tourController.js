d0d199c579aeb2ba312685eca45515723c7b5ee3
Marisa Ramon <mer0522@gmail.com>
2019-11-07 16:07:53 -0600

    create models folder to make mvc structure

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,18 +1,9 @@
-const fs = require('fs');
+// const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-exports.checkID = (req, res, next, val) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-    next();
-};
+// const tours = JSON.parse(
+//     fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+// );
 
 // checkBody middleware fxn to check if body contains the name and price properties
 // if not, send back 400 (bad request)
@@ -22,11 +13,10 @@ exports.checkBody = (req, res, next) => {
         return res.status(400).json({
             status: 'fail',
             message: 'Missing name or price'
-        })
+        });
     }
     next();
 };
-
 
 exports.getAllTours = (req, res) => {
     console.log(req.requestTime);
@@ -50,37 +40,41 @@ exports.getTour = (req, res) => {
         data: {
             tours
         }
-    })
+    });
 };
 
 exports.createTour = (req, res) => {
     // console.log(req.body);
-    exports.newId = tours[tours.length - 1].id + 1
+    exports.newId = tours[tours.length - 1].id + 1;
     exports.newTour = Object.assign({
-        id: newId
-    }, req.body);
+            id: newId
+        },
+        req.body
+    );
 
     tours.push(newTour);
 
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        })
-    })
+    fs.writeFile(
+        `${__dirname}/dev-data/data/tours-simple.json`,
+        JSON.stringify(tours),
+        err => {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    tour: newTour
+                }
+            });
+        }
+    );
 };
 
 exports.updateTour = (req, res) => {
-
-
     res.status(200).json({
         status: 'success',
         data: {
             tour: '<Updated tour here....>'
         }
-    })
+    });
 };
 
 exports.deleteTour = (req, res) => {
@@ -89,5 +83,5 @@ exports.deleteTour = (req, res) => {
         status: 'success',
         // data sent back is null
         data: null
-    })
+    });
 };
