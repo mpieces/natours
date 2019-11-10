ee31a9c91be02d92c433a5227b4ea0a988b64096
Marisa Ramon <mer0522@gmail.com>
2019-11-10 14:52:32 -0600

    make api better by enabling filtering

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const app = require('./app');
 
 dotenv.config({
     path: './config.env'
 });
+
+const app = require('./app');
+
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 mongoose
     .connect(DB, {
